660bc710643da06f59f8d8ff44a4083cccf34eb0
botniedata <botniedata@gmail.com>
2024-02-23 10:54:02 +0800

    initialize git local repo

---
+++ README.md
@@ -0,0 +1,22 @@
+
+- [x] wsl installation
+- [x] ubuntu update and upgrade
+- [x] vsftpd installation
+- [x] backup `etc/vsftpd.conf` file
+- [x] configure `etc/vsftpd.conf` file
+- [x] create `vsftpd.chroot_list`
+- [x] restart vsftpd services
+- [x] create non-sudo user
+- [x] create non-sudo user ftp directory
+- [x] change ownership for non-sudo user
+- [x] remove root access for non-sudo user
+- [x] add non-sudo user to `/etc/vsftpd.chroot_list` file
+- [x] create a FTP test connection in a simple Python script
+- [x] create python virtual environment
+- [x] activate the virtual environment for `requirements.txt`
+- [x] create file name as `requirements.txt`
+- [x] adds ftp credentials to virtual environment
+- [x] pip install `requirements.txt`
+- [x] activate the virtual environment to run the `app.py`
+
+- [ ] ftp connection successful
---
+++ app.py
@@ -0,0 +1,34 @@
+from ftplib import FTP_TLS
+
+# FTP details
+ftphost = "localhost" # replace with your FTP ip address or just localhost
+ftpuser = "ftpuser" # replace with your FTP username
+ftppass = "ftpuser!" # replace with your FTP password
+ftpport = 21
+
+# Login to FTP
+ftp = FTP_TLS()
+ftp.connect(ftphost, ftpport)
+ftp.login(ftpuser, ftppass)
+ftp.prot_p() # Fix on 522 Data connections must be encrypted.
+
+# Create sample file
+with open("test.txt", "w") as fp:
+    fp.write("This is my sample message\n")
+    fp.write("Another sample message\n")
+    fp.write("bye\n")
+
+# Upload file to FTP 
+with open("test.txt", "rb") as fp:
+    ftp.storbinary("STOR test_ftp.txt", fp)
+
+# Download file from FTP 
+with open("test_download.txt", "wb") as fp:
+    ftp.retrbinary("RETR test_ftp.txt",fp.write)
+
+# Delete file from FTP
+ftp.delete("test_ftp.txt")
+
+# If this line gets printed, you have successfully 
+# established connection to the WSL FTP Server. 
+print("Upload, download, delete tests complete")
