702a50147e4e3dd3650cdc296225526ecd5fafc3
botniedata <botniedata@gmail.com>
2024-02-28 09:15:47 +0800

    create ftp_ssl.py file for ssl testing

--- ftp_ssl.py
+++ ftp_ssl.py
@@ -7,6 +7,7 @@ import schedule
 import pandas as pd
 from os import environ, remove
 from pathlib import Path
+from OpenSSL import SSL # adds OpenSSL
 
 # ftp login credentials
 def get_ftp() -> FTP_TLS:
@@ -14,10 +15,16 @@ def get_ftp() -> FTP_TLS:
     FTPHOST = environ["FTPHOST"]
     FTPUSER = environ["FTPUSER"]
     FTPPASS = environ["FTPPASS"]
+    #KEYFILE = '/etc/ssl/private/vsftpd.pem'
+    #CERTFILE = '/etc/ssl/private/vsftpd.pem'
 
     # return authentication of FTP
     ftp = FTP_TLS(FTPHOST, FTPUSER, FTPPASS)
+    context = SSL.Context(SSL.SSLv23_METHOD) # adds SSL
+    ftp.ssl_version = SSL.SSLv23_METHOD
+    ftp.context()
     ftp.prot_p()
+    ftp.set_pasv(True) # adds passive mode to True
     return ftp
 
 # upload to ftp
@@ -27,6 +34,7 @@ def upload_to_ftp(ftp: FTP_TLS, file_source: Path):
         destination_directory = "/home/ftpuser/ftp"
         destination_path = f"{destination_directory}/{file_source.name}"
         ftp.storbinary(f"STOR {destination_path}", fp)  
+        ftp.quit()        # adds ftp.quit()
     
 def delete_file(file_source: str | Path):
     remove(file_source)
